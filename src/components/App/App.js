887a1fd675efc48b730fbfff696d32fe40be6f04
Desiree Quade <nekodez@gmail.com>
2022-01-30 14:05:15 -0600

    completed header and title component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+
+import Header from '../Header/Header';
 import MovieList from '../MovieList/MovieList'
 import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <Header />
       <Router>        
         <Route path="/" exact>
           <MovieList />
